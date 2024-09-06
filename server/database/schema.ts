5e407359fa00544075906503d0657a56cf24293b
SuperKojima <hayato.kojima@steamship.co.jp>
2024-09-06 01:20:32 +0000

    config

---
+++ server/database/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+
+export const users = pgTable('users', {
+    id: integer('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    avatar: text('avatar').notNull(),
+    createdAt: timestamp('created_at').notNull(),
+})
